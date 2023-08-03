ac17572c348f9adca4124b73cea99ea7c577c2b8
DaniMNehme <dani.m.nehme@gmail.com>
2023-08-03 20:20:38 +0300

    push

--- js/getProducts.js
+++ js/getProducts.js
@@ -267,33 +267,14 @@ fetch("https://aesthetic-eclair-56d00c.netlify.app/.netlify/functions/api/produc
               </div>
               <div class="card-body-a">
                 <div class="price-box d-flex">
-                  <span class="price-a">rent | ${product.price}</span>
+                  <span class="price-a">price ${product.price} $</span>
                 </div>
                 <a href="product-details.html?id=${product._id}" class="link-a">Click here to view
                   <span class="ion-ios-arrow-forward"></span>
                 </a>
               </div>
               <div class="card-footer-a">
-                <ul class="card-info d-flex justify-content-around">
-                  <li>
-                    <h4 class="card-info-title">Area</h4>
-                    <span>340m
-                      <sup>2</sup>
-                    </span>
-                  </li>
-                  <li>
-                    <h4 class="card-info-title">Beds</h4>
-                    <span>2</span>
-                  </li>
-                  <li>
-                    <h4 class="card-info-title">Baths</h4>
-                    <span>4</span>
-                  </li>
-                  <li>
-                    <h4 class="card-info-title">Garages</h4>
-                    <span>1</span>
-                  </li>
-                </ul>
+                
               </div>
             </div>
           </div>
